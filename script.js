9188a43c34c54f5cd8087e13d7d3b3d7b7a8a5aa
Marlon Pacheco <pacheco.mjc@gmail.com>
2020-08-12 19:55:13 -0500

    added search on click function with initial response

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Start Page
-    //API Kanye in container https://api.kanye.rest/
+//API Kanye in container https://api.kanye.rest/
 
 var kanyeQuote = function () {
     $.ajax({
@@ -8,7 +8,7 @@ var kanyeQuote = function () {
     }).then(function (response) {
         var quote = response.quote
         console.log(response)
-        $("#quoteKanye").text(quote)
+        $(".quoteKanye").text(quote)
 
     })
 };
@@ -17,10 +17,30 @@ kanyeQuote()
 
 
 //start button 
-        // hide first container and show search container
+// hide first container and show search container
 
 //Search Page
-    //Search on click
+//Search on click
+$("#searchBtn").on("click", function (event) {
+    // event.preventDefault() prevents the form from trying to submit itself.
+    event.preventDefault();
+        // This line will grab the text from the input box
+        var city = $("#city").val().trim();
+        // The City from the textbox is then added to our array
+        var state = $("#stacked-state").val().trim();
+        var postal = $("#zip").val();
+        var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + city + "&by_state" + state + "&by_postal" + postal;
+    
+        // Creating an AJAX call for the specific movie button being clicked
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            // calling renderButtons which handles the processing of our Cities array
+            console.log(response);
+    
+        });
+})
         //get values from input boxes - input.value
         //append https://api.openbrewerydb.org/breweries? + &by_city=(city input box) + &by_state=(state input box) &by_postal=(zip code input box) 
         //then show results page
@@ -35,5 +55,5 @@ kanyeQuote()
     //new search button on click to go back to search page
 
     //if error show "No Sway!" giphy
-        
-    
+
+
